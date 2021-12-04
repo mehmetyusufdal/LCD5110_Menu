2aa61cb7304f50a7f14c1fcf4263d698d5b7bd94
mehmetyusufdal <mehmetyusufdal@gmail.com>
2021-12-04 11:13:38 +0300

    Add new example

---
+++ examples/Navigate_on_Screens/Navigate_on_Screens.ino
@@ -0,0 +1,77 @@
+/*
+    --LCD5110_MENU by Mehmet Yusuf Dal--
+    This library is for generating a menu UI on Nokia 5110 GLCD display.
+
+    Adafruit_GFX & Adafruit_PCD8544 Libraries are required.
+    Sources: https://github.com/adafruit/Adafruit-GFX-Library
+             https://github.com/adafruit/Adafruit-PCD8544-Nokia-5110-LCD-library
+
+    ---------------------------------------------------------------------
+
+    This example shows how to navigate on different pages by using rotary encoder.
+
+    Arduino_Rotary_Encoder Library is required for this example.
+    Source: https://github.com/mehmetyusufdal/Arduino_Rotary_Encoder
+*/
+
+#include <LCD5110_Menu.h>
+#include <Encoder.h>
+#include "Pages.h"
+
+Adafruit_PCD8544 Screen::lcd = Adafruit_PCD8544(LCD_CLK, LCD_Din, LCD_DC, LCD_CE, LCD_RST); // Initialize Screen's static Adafruit_PCD8544 object.
+int Screen::active_page = -1; // -1 means menu page is displaying on the screen.
+
+Encoder encoder(ENC_CLK, ENC_DT, 0, 1, 0, 3); // Encoder generates values between 0-3 (0 and 3 included). Because menu has 4 options.
+
+String menu_items[4] = {"First", "Second", "Third", "About"}; // Menu item titles array.
+Menu menu("MENU", menu_items, 4);
+
+Pages pgs;
+
+bool bg_light = true; // Backlight state of the lcd.
+
+bool sw_last_state = false;
+bool buttonHandler();
+
+void setup(){
+    pinMode(ENC_SW, INPUT_PULLUP);
+    pinMode(LCD_BL, OUTPUT);
+    digitalWrite(LCD_BL, bg_light);
+
+    Serial.begin(9600);
+
+    Screen::lcd.begin();
+    Screen::lcdReset();
+
+    menu.showScreen(); // Show menu on display.
+
+}
+
+void loop(){
+    // If the encoder's state changes, update chosen menu item and refresh menu.
+    if(Screen::active_page == -1 && encoder.stateControl()){
+        menu.setChosen(encoder.getState());
+        menu.showScreen();
+    }
+
+    if(buttonHandler()){
+        if(Screen::active_page != -1){ // If active page is not menu, return to menu.
+            Screen::active_page = -1;
+            menu.showScreen();
+        }
+        else{ // Go to the selected page on menu.
+            Screen::active_page = encoder.getState();
+            pgs.showScreen();
+        }
+    }
+    
+
+}
+
+bool buttonHandler(){
+    bool sw_state = false;
+    if(!digitalRead(ENC_SW) && !sw_last_state) sw_state = true;
+    sw_last_state = !digitalRead(ENC_SW);
+
+    return sw_state;
+}
---
+++ examples/Navigate_on_Screens/Pages.h
@@ -0,0 +1,78 @@
+#ifndef _PAGES_H_
+#define _PAGES_H_
+
+#include <LCD5110_Screen.h>
+
+class Pages : public Screen{
+private:
+    void firstScr(){
+        String txt = "First Page";
+
+        lcdReset();
+        lcd.setCursor(textAlign(CENTER_X, &txt), textAlign(CENTER_Y, &txt));
+        lcd.print(txt);
+        lcd.display();
+    }
+
+    void secondScr(){
+        String txt = "Second PAGE";
+
+        lcdReset();
+        lcd.setCursor(textAlign(CENTER_X, &txt), textAlign(CENTER_Y, &txt));
+        lcd.print(txt);
+        lcd.display();
+    }
+
+    void thirdScr(){
+        String txt = "Third PAGE";
+
+        lcdReset();
+        lcd.setCursor(textAlign(CENTER_X, &txt), textAlign(CENTER_Y, &txt));
+        lcd.print(txt);
+        lcd.display();
+    }
+
+    void aboutScr(){
+        String title = "About";
+        String project = "LCD5110_Menu";
+        String creator_name = "Mehmet Yusuf";
+        String creator_surname = "Dal";
+
+        lcdReset();
+        lcd.setCursor(textAlign(CENTER_X, &title), 0);
+        lcd.println(title);
+        lcd.drawLine(0, lcd.getCursorY() + 1, 83, lcd.getCursorY() + 1, BLACK);
+
+        lcd.setCursor(textAlign(CENTER_X, &project), lcd.getCursorY() + 9);
+        lcd.println(project);
+
+        lcd.setCursor(textAlign(CENTER_X, &creator_name), lcd.getCursorY() + 8);
+        lcd.println(creator_name);
+
+        lcd.setCursor(textAlign(CENTER_X, &creator_surname), lcd.getCursorY());
+        lcd.println(creator_surname);
+
+        lcd.display();
+    }
+
+public:
+    void showScreen(){
+        switch(active_page){
+            case 0:
+                firstScr();
+                break;
+            case 1:
+                secondScr();
+                break;
+            case 2:
+                thirdScr();
+                break;
+            case 3:
+                aboutScr();
+                break;
+        }
+    }
+
+};
+
+#endif
